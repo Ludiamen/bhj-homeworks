139a756c2f8a1acd9bcbbcdd39fcc456c977ad3a
Ludiamen <maxvas1001@gmail.com>
2022-10-15 17:38:31 +0300

    solved the first three tasks

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,14 @@
+const cookie = document.getElementById("cookie");
+const clickerCounter = document.getElementById("clicker__counter");
+
+
+cookie.onclick = function() {
+    let clicks = clickerCounter.textContent++;
+    if (clicks % 2 == 0) {
+        cookie.width = 150;
+        cookie.height = 150;
+    } else {
+        cookie.width = 200;
+        cookie.height = 200;
+    }
+}
